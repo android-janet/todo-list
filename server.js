37893e3b7cf521062eaea56eac3173df745dd549
Melissa Khat <melissa.khat@gmail.com>
2018-03-21 14:38:55 -0600

    update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var TODOS_COLLECTION = "todos";
 var app = express();
 app.use(bodyParser.json());
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
